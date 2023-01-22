a297c2c3e8a968998edd8c480c432aefd377c757
Seda <52498111+sedadenboer@users.noreply.github.com>
2023-01-22 16:12:25 +0100

    clean up assimilation and contrast function

--- code/agents.py
+++ code/agents.py
@@ -4,10 +4,9 @@ from scipy.spatial import distance
 import numpy as np
 import copy
 
+
 class Wappie(Agent):
-    def __init__(
-        self, unique_id: int, model: Model, grid_pos, prior_beliefs
-    ) -> None:
+    def __init__(self, unique_id: int, model: Model, grid_pos, prior_beliefs) -> None:
         super().__init__(unique_id, model)
 
         self.beliefs = prior_beliefs
@@ -17,64 +16,54 @@ class Wappie(Agent):
         # print(self.pos)
 
     def distance(self, other):
+        """ """
         # print(self.beliefs, other.beliefs)
         return distance.euclidean(list(self.beliefs), list(other.beliefs))
 
     def assimilation(self, other):
         """ """
+        # make a copy of beliefs
         beliefs_copy = copy.copy(self.beliefs)
-        interacting_neighbor_beliefs_copy = copy.copy(other.beliefs)
+        neighbor_beliefs_copy = copy.copy(other.beliefs)
 
-        beliefs_copy += self.model.mu * np.subtract(
-            self.beliefs, other.beliefs
-        )
-        interacting_neighbor_beliefs_copy += self.model.mu * np.subtract(
-            other.beliefs, self.beliefs
-        )
-
+        # update beliefs of agent and interacting connection
+        beliefs_copy += self.model.mu * (self.beliefs - other.beliefs)
+        neighbor_beliefs_copy += self.model.mu * (other.beliefs - self.beliefs)
         self.beliefs = beliefs_copy
-        other.beliefs = interacting_neighbor_beliefs_copy
+        other.beliefs = neighbor_beliefs_copy
 
     def normalisation(self, x):
+        """ """
         if x < 0.0:
-            return 0.0 # should be float, otherwise numpy will scream at you...
+            return 0.0  # should be float, otherwise numpy will scream at you...
         elif x > 1.0:
             return 1.0
         else:
             return x
 
     def contrast(self, other):
+        # make a copy of beliefs
+        beliefs_copy = copy.copy(self.beliefs)
+        neighbor_beliefs_copy = copy.copy(other.beliefs)
 
-        prog_cons_1 = self.beliefs[0]
-        left_right_1 = self.beliefs[1]
+        # vectorize normalisation vector s.t. it can be applied to an numpy array
+        add_normalisation = np.vectorize(add_normalisation)
 
-        prog_cons_2 = other.beliefs[0]
-        left_right_2 = other.beliefs[1]
-
-        new_prog_cons_1 = self.normalisation(
-            prog_cons_1 - self.model.lambd * (prog_cons_2 - prog_cons_1)
-        )
-        new_prog_cons_2 = self.normalisation(
-            prog_cons_2 - self.model.lambd * (prog_cons_1 - prog_cons_2)
-        )
-
-        new_left_right_1 = self.normalisation(
-            left_right_1 - self.model.lambd * (left_right_2 - left_right_1)
-        )
-        new_left_right_2 = self.normalisation(
-            left_right_2 - self.model.lambd * (left_right_1 - left_right_2)
-        )
-
-        self.belief = np.array([new_prog_cons_1, new_left_right_1])
-        other.beliefs = np.array(
-            [new_prog_cons_2, new_left_right_2]
-        )
+        # update beliefs of agent and interacting connection
+        beliefs_copy += add_normalisation(
+            self.beliefs - self.model.lambd * (other.beliefs - self.beliefs)
+            )
+        neighbor_beliefs_copy += add_normalisation(
+            other.beliefs - self.model.lambd * (self.beliefs - other.beliefs)
+            )
+        self.beliefs = beliefs_copy
+        other.beliefs = neighbor_beliefs_copy
 
     def step(self):
         # get neighbors from grid and network
         neighbors_grid = self.model.grid.get_neighbors(self.grid_pos, moore=True)
         # print(f"grid: {neighbors_grid}")
-        connected_nodes = self.model.network.get_neighbors(self.unique_id) 
+        connected_nodes = self.model.network.get_neighbors(self.unique_id)
         # lol these are node ids
         neighbors_network = [self.model.agents[i] for i in connected_nodes]
         # print(f"network: {neighbors_network}")
